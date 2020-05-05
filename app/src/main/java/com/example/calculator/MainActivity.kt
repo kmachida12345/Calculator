df39ac573d437d19ee0cae6e61635ce62abd95f6
kmachida <kmachida12345@gmail.com>
2020-05-05 15:07:11 +0900

    feat: 🎸 DataBinding導入

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.example.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var calcViewModel: CalcViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // ViewModelを取得
+        calcViewModel = ViewModelProvider(this).get(CalcViewModel::class.java)
+
+        // でーたばいんでぃんぐの設定
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.viewModel = calcViewModel
+        binding.lifecycleOwner = this
     }
 }
