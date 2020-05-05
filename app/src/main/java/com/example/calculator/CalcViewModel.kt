46ffcb14770568b6c8fc382bc23fb0113bd7ab06
kmachida <kmachida12345@gmail.com>
2020-05-05 16:04:29 +0900

    feat: 🎸 ボタン１と＋にクリックリスナーを実装

--- app/src/main/java/com/example/calculator/CalcViewModel.kt
+++ app/src/main/java/com/example/calculator/CalcViewModel.kt
@@ -1,18 +1,36 @@
 package com.example.calculator
 
+import android.util.Log
+import android.view.View
+import android.widget.Button
 import androidx.lifecycle.ViewModel
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class CalcViewModel: ViewModel() {
-    var operator: String = ""
     var answer: Double = 0.0
-    var operand: Double = 0.0
+    var operand: String = "0"
+    var operator: String = ""
+    var formula: String = "aaaa"
 
     fun calc(){
         when (operator){
-            "+" -> answer += operand
-            "-" -> answer -= operand
-            "/" -> answer /= operand
-            "*" -> answer *= operand
+            "+" -> answer += operand.toDouble()
+            "-" -> answer -= operand.toDouble()
+            "/" -> answer /= operand.toDouble()
+            "*" -> answer *= operand.toDouble()
+            else -> answer = operand.toDouble()
         }
+    }
+
+    fun onNumButtonClicked(num: View){
+        val button = num as Button
+        formula = button.text.toString()
+        calc()
+        Log.d("hoge", "onNumButtonClicked() called $num$formula${button.text}")
+    }
+
+    fun onOperatorButtonClicked(ope: View){
+        formula += ope
+        operator = ope.toString()
     }
 }
